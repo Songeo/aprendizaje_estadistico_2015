2a02c213a9209ee12fce90287c6b61f07dd08625
Felipe Gonzalez <felipexgonzalez@gmail.com>
2015-10-27 19:55:11 -0600

    Cambios finales de concurso

--- clases/concurso_movimiento/1_prep_archivos_concurso.R
+++ clases/concurso_movimiento/1_prep_archivos_concurso.R
@@ -11,29 +11,70 @@ nrow(datos_f)
 
 datos_f$hora <- hour(datos_f$datetime)
 datos_f$dia_sem <- factor(weekdays(datos_f$datetime))
-datos_b <- datos_f
-table(datos_b$puerta_pir)
+datos_b <- filter(datos_f, estudiof < 0.5)
+table(datos_b$estudiof_pir)
 datos_b$puerta_m <- 'Activado'
-datos_b$puerta_m[!datos_b$puerta_pir] <- 'Desactivado'
+datos_b$puerta_m[!datos_b$estudiof_pir] <- 'Desactivado'
 datos_b$puerta_m <- factor(datos_b$puerta_m)
 table(datos_b$puerta_m)
-
-
+datos_b$ind <- 1:nrow(datos_b)
+quantile(datos_b$ind)
 datos_b$muestra <- 'entrena'
-datos_b$muestra[datos_b$ind > 150000] <- 'publico'
-datos_b$muestra[datos_b$ind > 175000] <- 'privado'
+datos_b$muestra[datos_b$ind > 70000] <- 'publico'
+datos_b$muestra[datos_b$ind > 90000] <- 'privado'
 entrena  <- filter(datos_b, muestra == 'entrena')
 publicos <- filter(datos_b, muestra == 'publico')
 privados <- filter(datos_b, muestra == 'privado')
+nrow(entrena)
+nrow(publicos)
+nrow(privados)
+table(entrena$puerta_m)
 table(publicos$puerta_m)
 table(privados$puerta_m)
 
 set.seed(391)
-datos_1 <- entrena %>% filter(puerta_pir==1)
-datos_2 <- entrena %>% filter(puerta_pir==0) %>% sample_n(nrow(datos_1))
+datos_1 <- entrena %>% filter(estudiof_pir==1)
+datos_2 <- entrena %>% filter(estudiof_pir==0) %>% sample_n(nrow(datos_1))
 datos_t <- bind_rows(datos_1, datos_2)
 #datos_t <- datos_b
 nrow(datos_t)
-prop.table(table(datos_t$puerta_pir))
+prop.table(table(datos_t$estudiof_pir))
 table(datos_t$puerta_m)
 
+
+entrena_subir <- entrena %>% dplyr::select(datetime:vestidor, consumo,  cocina_pir, sala_pir, estudiof_pir)
+publico_f <- entrena %>% dplyr::select(datetime:vestidor, consumo, cocina_pir, sala_pir, estudiof_pir)
+privado_f <- entrena %>% dplyr::select(datetime:vestidor, consumo, cocina_pir, sala_pir, estudiof_pir)
+eval <- bind_rows(publico_f, privado_f)
+
+head(eval)
+solucion <- eval %>% dplyr::select(cocina_pir, sala_pir, estudiof_pir) %>%
+  mutate(cocina_pir = as.numeric(cocina_pir), 
+         sala_pir=as.numeric(sala_pir),
+         estudiof_pir = as.numeric(estudiof_pir))
+solucion$id <- 1:nrow(solucion)
+solucion <- solucion %>% dplyr::select(id, cocina_pir, sala_pir, estudiof_pir) 
+write_csv(solucion, './datos/solucion.csv')
+
+example_sub <- solucion
+example_sub$cocina_pir <- 0
+example_sub$sala_pir <- 0
+example_sub$estudiof_pir <- 0
+write_csv(example_sub, './datos/sample_sub.csv')
+
+entrena_s <- entrena_subir %>% 
+  mutate(cocina_pir = as.numeric(cocina_pir), 
+         sala_pir=as.numeric(sala_pir),
+         estudiof_pir = as.numeric(estudiof_pir))
+prueba_s <- eval %>%
+  mutate(cocina_pir = as.numeric(cocina_pir), 
+         sala_pir=as.numeric(sala_pir),
+         estudiof_pir = as.numeric(estudiof_pir)) %>% 
+  dplyr::select(-cocina_pir, -sala_pir, -estudiof_pir)
+
+write_csv(prueba_s, './datos/test.csv')
+
+
+write_csv(entrena_s, './datos/train.csv')
+
+write_csv(prueba_s, './datos/test.csv')
--- clases/concurso_movimiento/pruebas.R
+++ clases/concurso_movimiento/pruebas.R
@@ -5,19 +5,40 @@ library(tidyr)
 library(lubridate)
 library(randomForest)
 
-rf <- randomForest(puerta_m ~ bano_cuarto+bano_escalera+cocina+
-                     cuarto+entrada+escalera+estudiof+estudiot+puerta+sala+tv+vestidor+
-                     consumo + hora + dia_sem, 
-                   data=datos_t, 
+entrena_s
+
+rf_1 <- randomForest(factor(cocina_pir) ~ cocina, 
+                   data=entrena_s, 
                    ntree=500, importance = T)
-rf
+rf_2 <- randomForest(factor(sala_pir) ~ sala, 
+                     data=entrena_s,
+                     ntree=500, importance = T)
+rf_3 <- randomForest(factor(estudiof_pir) ~ estudiof, 
+                     data=entrena_s,
+                     ntree=500, importance = T)
+
+pred_cocina <- predict(rf_1, eval, type ='prob')[,2]
+pred_sala <- predict(rf_2, eval, type ='prob')[,2]
+pred_estudiof <- predict(rf_2, eval, type ='prob')[,2]
+
+sub <- example_sub
+sub$cocina_pir <- pred_cocina
+sub$sala_pir <- pred_sala
+sub$estudiof_pir <- pred_estudiof
+write_csv(sub, 'data/bench_1.csv')
 plot(rf)
 importance(rf, scale= F)
 
-publicos <- filter(datos_b, muestra=='publico')
-table(pp <- predict(rf, publicos))
+table(pp <- predict(rf, publicos, type='prob')[,1] > 0.5)
+publicos_2 <- publicos
+publicos$pred <- predict(rf, publicos, type='prob')[,1]
+#ggplot(publicos, aes(x=estudiof, y=pred, colour=puerta_m)) + geom_point() +
+#  facet_wrap(~puerta_m) + geom_smooth()
+
 table(pp, publicos$puerta_m)
 prop.table(table(pp, publicos$puerta_m),2)
+table(pp, publicos$puerta_m, cut(publicos$estudiof, seq(0,1,0.25)))
+
 
 privados <- filter(datos_b, muestra=='privado')
 table(pp <- predict(rf, privados))
